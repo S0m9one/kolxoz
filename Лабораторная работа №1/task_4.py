497043f5575075dde618a95ada75c19e07788496
S0m9one <133509658+S0m9one@users.noreply.github.com>
2025-12-08 15:04:18 -0500

    Лабораторная работа 1

---
+++ Лабораторная работа №1/task_4.py
@@ -0,0 +1,9 @@
+HOURS_IN_DAY = 24  # количество часов в дне постоянно
+total_hours = 5000  # дано по задаче
+
+days =total_hours//24   # TODO найти полное количество дней
+hours =total_hours % 24
+
+
+print("Количество дней =", days)
+print("Оставшееся количество часов =", hours)
